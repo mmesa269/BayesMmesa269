700023fa085079f480b7538442f29269ed8e1d55
mmesa269 <110608005+mmesa269@users.noreply.github.com>
2025-02-26 14:19:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 def Teorema_bayes(Aprior, Sensitividad, Specificidad):
-    """Uso del Teorema de Bayes para determinar el chance de tener covid si se stiene un test positivo"""
+    """Teorema de Bayes para determinar el chance de tener covid si se stiene un test positivo"""
     Falsos_pos = 1 - Specificidad
     p_b = (Sensitividad * Aprior) + (Falsos_pos * (1 - Aprior))
     aposterior = (Sensitividad * Aprior) / p_b
     return aposterior
 
-st.title("Bayes' Theorem: COVID-19 Test Probability Calculator")
+st.title("Uso del Teorema de Bayes para determinar el chance de tener covid si se stiene un test positivo")
 
 # User inputs
 Aprior = st.slider("Probabilidad de tener covid [P(A)]", 0.01, 1.0, 0.04, 0.01)
